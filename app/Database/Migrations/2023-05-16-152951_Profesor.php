f7716d2b39b754673e2daefe4e6e7459a42c466b
edgardegantea <edgar.degante.a@gmail.com>
2023-05-25 22:01:08 -0600

    Actualización

--- app/Database/Migrations/2023-05-16-152951_Profesor.php
+++ app/Database/Migrations/2023-05-16-152951_Profesor.php
@@ -8,16 +8,22 @@ class Profesor extends Migration
 {
     public function up()
     {
+        $this->db->disableForeignKeyChecks();
+
         $this->forge->addField([
             'id'                => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
             'nombre'            => ['type' => 'varchar', 'constraint' => 50],
             // 'apellidoPaterno'   => ['type' => 'varchar', 'constraint' => 50],
             // 'apellidoMaterno'   => ['type' => 'varchar', 'constraint' => 50],
             'foto'              => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
+            'area'              => ['type' => 'int', 'constraint' => 11, 'auto_increment' => false, 'unsigned' => true]
         ]);
 
         $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('area', 'areas', 'id');
         $this->forge->createTable('profesores', true);
+        
+        $this->db->enableForeignKeyChecks();
     }
 
     public function down()
